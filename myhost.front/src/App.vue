07044b00891638a20ef702c1a9aed3078df019e4
Olarn Rojanapornpun <olarnr@users.noreply.github.com>
2023-03-28 09:49:00 +0700

    fix adding of frontend

---
+++ myhost.front/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+const API_URL = `http://lvmolarn.sit.kmutt.ac.th:50000/api/host/hostname`
+
+export default {
+  data: () => ({
+    date: null
+  }),
+
+  created() {
+    // fetch on init
+    this.fetchData()
+  },
+
+  methods: {
+    async fetchData() {
+      const url = `${API_URL}`
+      this.hostname = await (await fetch(url)).json()
+    }
+  }
+}
+</script>
+
+<template>
+  <p>{{ hostname }}</p>
+</template>
